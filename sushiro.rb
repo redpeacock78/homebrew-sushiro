fe6d618c90ddfecb734c68cc5f7b81d09dd4561d
redpeacock78 <scarletpeacock78@gmail.com>
2018-12-03 20:52:19 +0900

    add new files

--- sushiro.rb
+++ sushiro.rb
@@ -2,6 +2,7 @@ class Sushiro < Formula
   homepage "https://github.com/redpeacock78"
   url "https://raw.githubusercontent.com/redpeacock78/sushiro/master/sushiro"
   sha256 'f9099cdbd2692cd8cb5406c7c6af0d6b7ff574c4e43f217b53b0355848f747b1'
+  version "0.0.1"
 
   def install
     bin.install "sushiro"
