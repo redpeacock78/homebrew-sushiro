1e25364c10ef33f0a6912b0675b364bed97f7f33
redpeacock78 <scarletpeacock78@gmail.com>
2018-12-04 17:36:32 +0900

    update

--- sushiro.rb
+++ sushiro.rb
@@ -1,4 +1,5 @@
 class Sushiro < Formula
+  desc "Randomly display the menu of 'Sushiro'"
   homepage "https://github.com/redpeacock78"
   url "https://raw.githubusercontent.com/redpeacock78/sushiro/master/sushiro"
   sha256 '8692623437a00e76dc184de74d275a6b2e1a4d2c69740c312b2340c15baa5180'
