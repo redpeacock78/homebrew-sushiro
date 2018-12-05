f6d7c49e0b25c9a61509ecb153e96fab63f0d7ec
redpeacock78 <scarletpeacock78@gmail.com>
2018-12-05 15:44:21 +0900

    update

--- sushiro.rb
+++ sushiro.rb
@@ -5,7 +5,7 @@ class Sushiro < Formula
   homepage "https://github.com/redpeacock78"
   url "https://github.com/redpeacock78/sushiro/releases/download/#{FORMULA_VERSIONS}/sushiro"
   sha256 "ad73e661e57b37be36b1bc99a18eb2578f79d57ee65f6b7da807772b42c2e99c"
-  head "https://github.com/redpeacock78/homebrew-sushiro.git", :branch => "master"
+  head "https://github.com/redpeacock78/homebrew-sushiro.git", :tag => "#{FORMULA_VERSIONS}"
 
   def install
     bin.install "sushiro"
