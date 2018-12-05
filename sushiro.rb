a4351d8179699fcb61dd186c892e61429163e392
redpeacock78 <scarletpeacock78@gmail.com>
2018-12-05 15:08:45 +0900

    update

--- sushiro.rb
+++ sushiro.rb
@@ -1,11 +1,9 @@
 class Sushiro < Formula
   desc "Randomly display the menu of 'Sushiro'"
   homepage "https://github.com/redpeacock78"
-  head "https://github.com/redpeacock78/homebrew-sushiro.git"
-  version '0.0.4'
   url "https://github.com/redpeacock78/sushiro/releases/download/#{version}/sushiro"
-  sha256 'ad73e661e57b37be36b1bc99a18eb2578f79d57ee65f6b7da807772b42c2e99c'
-
+  sha256 "ad73e661e57b37be36b1bc99a18eb2578f79d57ee65f6b7da807772b42c2e99c"
+  head "https://github.com/redpeacock78/homebrew-sushiro.git"
   def install
     bin.install "sushiro"
   end
