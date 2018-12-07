30ea0b827e4e04b484eb43ff5cf47955b49d15db
redpeacock78 <scarletpeacock78@gmail.com>
2018-12-08 01:22:23 +0900

    sushiro is updated

--- sushiro.rb
+++ sushiro.rb
@@ -4,7 +4,7 @@ class Sushiro < Formula
   desc "🍣 Randomly display the menu of 'Sushiro' 🍣"
   homepage "https://github.com/redpeacock78"
   url "https://github.com/redpeacock78/sushiro/releases/download/#{FORMULA_VERSIONS}/sushiro"
-  sha256 "bd825938f415bd972a803b8b717147e75e75caa5c46e52934f49d42c652f1ef4"
+  sha256 "f15a08dc9cf552172984d8aae168eff9041679e8fa5644e8250e3144e5890d7c"
   head "https://github.com/redpeacock78/sushiro.git", :branch => "master"
 
   def install
