83bf209c5769f514cab0d46efc30eda4e81a736e
redpeacock78 <scarletpeacock78@gmail.com>
2018-12-09 20:35:04 +0900

    update

--- sushiro.rb
+++ sushiro.rb
@@ -1,12 +1,13 @@
 FORMULA_VERSIONS="0.0.5".freeze
+REPOSITORY_URL="https://github.com/redpeacock78/sushiro/"
 
 class Sushiro < Formula
   desc "🍣 Randomly display the menu of 'Sushiro' 🍣"
   homepage "https://github.com/redpeacock78"
-  url "https://github.com/redpeacock78/sushiro/releases/download/#{FORMULA_VERSIONS}/sushiro"
+  url "#{REPOSITORY_URL}/releases/download/#{FORMULA_VERSIONS}/sushiro"
+  version FORMULA_VERSIONS
   sha256 "fd49f9cd33fbc91bdbf8c0472978b410adea93b888d2a55a895bae5c9682ea3e"
   head "https://github.com/redpeacock78/sushiro.git", :branch => "master"
-  version FORMULA_VERSIONS
 
   def install
     bin.install "sushiro"
