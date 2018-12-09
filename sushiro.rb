48690a31e7a7cf0da231e4c31c9887a7c64c9a04
redpeacock78 <scarletpeacock78@gmail.com>
2018-12-09 20:48:52 +0900

    update

--- sushiro.rb
+++ sushiro.rb
@@ -4,7 +4,8 @@ REPOSITORY_URL="https://github.com/redpeacock78/sushiro".freeze
 class Sushiro < Formula
   desc "🍣 Randomly display the menu of 'Sushiro' 🍣"
   homepage "https://github.com/redpeacock78"
-  url "#{REPOSITORY_URL}/releases/download/#{FORMULA_VERSIONS}/sushiro"
+  #url "#{REPOSITORY_URL}/releases/download/#{FORMULA_VERSIONS}/sushiro"
+  url REPOSITORY_URL, tag => FORMULA_VERSION
   version FORMULA_VERSIONS
   sha256 "fd49f9cd33fbc91bdbf8c0472978b410adea93b888d2a55a895bae5c9682ea3e"
   head REPOSITORY_URL, :branch => "master"
