9b0f6e71e282a50bbfd44a4c8c51fd818f376687
redpeacock78 <scarletpeacock78@gmail.com>
2018-12-09 21:10:39 +0900

    update

--- sushiro.rb
+++ sushiro.rb
@@ -5,8 +5,8 @@ class Sushiro < Formula
   desc "🍣 Randomly display the menu of 'Sushiro' 🍣"
   homepage "https://github.com/redpeacock78"
   url REPOSITORY_URL,
-    :tag => FORMULA_VERSIONS,
-    :revision => "44b64d998dd1c24250c9f64655451d078b0e81bd"
+  :tag => FORMULA_VERSIONS,
+  :revision => "44b64d998dd1c24250c9f64655451d078b0e81bd"
   sha256 "fd49f9cd33fbc91bdbf8c0472978b410adea93b888d2a55a895bae5c9682ea3e"
   head REPOSITORY_URL, :branch => "master"
 
