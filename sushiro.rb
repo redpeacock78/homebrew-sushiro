d9fcf4efa51751975934b6cb180f4d9cc94ca417
redpeacock78 <scarletpeacock78@gmail.com>
2018-12-09 20:59:52 +0900

    update

--- sushiro.rb
+++ sushiro.rb
@@ -6,7 +6,7 @@ class Sushiro < Formula
   homepage "https://github.com/redpeacock78"
 #url "#{REPOSITORY_URL}/releases/download/#{FORMULA_VERSIONS}/sushiro"
   url REPOSITORY_URL, 
-    :tag => FORMULA_VERSIONS
+    :tag => FORMULA_VERSIONS,
     :revision => "44b64d998dd1c24250c9f64655451d078b0e81bd"
 #version FORMULA_VERSIONS
   sha256 "fd49f9cd33fbc91bdbf8c0472978b410adea93b888d2a55a895bae5c9682ea3e"
