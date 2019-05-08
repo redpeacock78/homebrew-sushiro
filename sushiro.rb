ea7befd328ef27406e957347017bc8cc66bd345c
redpeacock78 <redpeacock78@Macbook.local>
2019-05-08 19:05:46 +0900

    Update

--- sushiro.rb
+++ sushiro.rb
@@ -5,7 +5,7 @@ class Sushiro < Formula
   homepage "https://github.com/redpeacock78"
   url "https://github.com/redpeacock78/sushiro.git"
   head "https://github.com/redpeacock78/sushiro.git", :branch => "master"
-  version '0.0.6'.freeze
+  version '0.0.7'.freeze
   # sha256 "847696fef76f6d63b70749590a1af55695cb30bef42cd984146d60c7bf6d4a42"
 
   bottle :unneeded
